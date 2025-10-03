d00d9e537f69b4b1260269acf335bbf4ee6923d7
Desmond Opoku Anane <136804648+doanane@users.noreply.github.com>
2025-10-03 19:01:24 +0000

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://skimmer.pythonanywhere.com/api",
+  baseURL: "https://yosa.onrender.com",
 });
 
 export default instance;
